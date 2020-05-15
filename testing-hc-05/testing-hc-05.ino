eeebae5221dff9178e911250f1d09ac8a397282d
Andrew Hamilton <andrew.w.hamilton@strath.ac.uk>
2020-05-15 10:25:08 +0100

    added some other examples

---
+++ testing-hc-05/testing-hc-05.ino
@@ -0,0 +1,46 @@
+#include <SoftwareSerial.h>// import the serial library
+
+ 
+SoftwareSerial BT(10, 11); // RX, TX
+int ledpin=13; // led on D13 will show blink on / off
+int BluetoothData; // the data given from Computer
+int SerialValue;
+
+void setup()
+{
+
+  BT.begin(9600);
+  BT.println("Bluetooth On please press 1 or 0 blink LED ..");
+  pinMode(ledpin,OUTPUT);
+}
+ 
+void loop()
+{
+
+  if (BT.available())
+  {
+    BluetoothData=BT.read();
+    if(BluetoothData=='1')
+    {
+      // if number 1 pressed ....
+      digitalWrite(ledpin,1);
+      BT.println("LED  On D13 ON ! ");
+    }
+    if (BluetoothData=='0')
+    {
+      // if number 0 pressed ....
+      digitalWrite(ledpin,0);
+      BT.println("LED  On D13 Off ! ");
+    }
+    if (BluetoothData=='2')
+    {
+      // if number 0 pressed ....
+      SerialValue = BT.read();
+      BT.print("RSSI: ");
+      BT.println(SerialValue);
+    }
+  }
+delay(100);// prepare for next data ...
+}
+
+//      BT.println(BT.rssi())
