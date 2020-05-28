63b50f60cbc196c14307646b0e0551e7d831725c
djb20103 <paul.mchard@strath.ac.uk>
2020-05-28 09:22:45 +0100

    LED test added to archive

---
+++ archive/led_pinout_test/led_pinout_test/led_pinout_test.ino
@@ -0,0 +1,40 @@
+
+#include <SoftwareSerial.h>
+int flag;
+int lastFlag;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+    Serial.print("Set LED HIGH (1) or LOW (0)\r\n");
+    delay(100);
+    flag = -1;
+    lastFlag = -1;
+}
+
+void loop() {
+  if(Serial.available() > 0)  {
+    flag= Serial.read()-'0'; // can be -1 if read error
+    if(flag != lastFlag){
+      switch(flag) {
+          case 1:
+             digitalWrite(13,HIGH);
+             Serial.print("Setting LED to HIGH\r\n");
+             lastFlag = flag;
+          break;
+
+          case 0:
+             digitalWrite(13,LOW);
+             Serial.print("Setting LED to LOW\r\n");
+             lastFlag = flag;
+          break;
+
+          default:
+            break;
+      } //end switch case
+   } //end interior if
+   else{
+    Serial.print("No Change!\r\n");
+   }
+ } //end exterior if
+}
